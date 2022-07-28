4c30c853e95149246b953f2fc9c9f17cc11096bb
TE-KazuhiroYabe <Kazuhiro.Yabe@sony.com>
2022-07-28 15:18:01 +0900

    Update index.htm

--- docs/downloadImage.js
+++ docs/downloadImage.js
@@ -1,10 +1,16 @@
 async function downloadImage(url) {
+	const downloadButton = document.querySelector('#download-button');
+	downloadButton.disabled = true;
+
 	const image = await fetch(url, { mode: 'cors' });
 	const blob = await image.blob();
+	const blobData = await blob.text();
 
-	console.log(blob)
+	console.log(blobData)
 
-	await download('data:image/png;base64,' + blob, 'toio_id_4x4.png', 'image/png');
+	await download('data:image/png;base64,' + blobData, 'toio_id_4x4.png', 'image/png');
+
+	downloadButton.disabled = false;
 }
 
 
