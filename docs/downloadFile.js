d7f4329c6e29298d43add91d9490f16925f8d83e
TE-KazuhiroYabe <Kazuhiro.Yabe@sony.com>
2022-07-29 14:10:11 +0900

    Update

---
+++ docs/downloadFile.js
@@ -0,0 +1,27 @@
+async function downloadPDF(url) {
+	const downloadButton = document.querySelector('#download-button');
+	const messageArea = document.querySelector('#message-area');
+
+	downloadButton.disabled = true;
+	messageArea.innerHTML = "Generating now...";
+
+	try {
+		const image = await fetch(url, { mode: 'cors' });
+		const blob = await image.blob();
+		const blobData = await blob.text();
+
+		messageArea.innerHTML = "data received";
+		console.log(blobData)
+
+		messageArea.innerHTML = "save file";
+		await download('data:application/pdf;base64,' + blobData, 'toio_id_4x4.pdf', 'application/pdf');
+		messageArea.innerHTML = "complete";
+	} catch (e) {
+		messageArea.innerHTML = e.toString();
+		console.error(e);
+	} finally {
+		downloadButton.disabled = false;
+	}
+}
+
+
